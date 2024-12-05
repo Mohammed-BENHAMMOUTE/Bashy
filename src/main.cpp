517924ab17c90341e814ba9c49f2dfad70867ff5
Mohammed-BENHAMMOUTE <mohammedbenhammoute@gmail.com>
2024-12-06 00:23:58 +0100

    adding echo comand

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
+#include <algorithm>
 #include <iostream>
+using namespace std;
 
+std::pair<std::string,std::string> processString(std::string& s) {
+    std::pair<std::string, std::string> p;
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
+    auto it  = s.find(' ');
+    if (it != std::string::npos) {
+        p.first = s.substr(0, it);
+        p.second = s.substr(it + 1);
+    } else {
+        p.first = s;
+    }
+    return p;
+}
 int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
@@ -9,10 +28,14 @@ int main() {
         getline(std::cin, input);
         if (input == "exit 0") {
             exit(0);
-            return 0;
         }
-        if (!input.empty()) {
+        pair<string, string> processedstring = processString(input);
+        if (processedstring.first == "echo") {
+            cout << processedstring.second<<endl;
+        }else if (!input.empty()) {
             std::cout << input << ": command not found" << std::endl;
         }
     }
 }
+
+
