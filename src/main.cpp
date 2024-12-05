1c3e2a63af97a7582995bbac28a3ce5eaec7c688
Mohammed-BENHAMMOUTE <mohammedbenhammoute@gmail.com>
2024-12-05 22:48:52 +0100

    adding exit status

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 
 int main() {
-  // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
    std::string input;
     while (true) {
         std::cout << "$ ";
         getline(std::cin, input);
+        if (input == "exit 0") {
+            exit(0);
+            return 0;
+        }
         if (!input.empty()) {
-            // std::getline(std::cin, input);
             std::cout << input << ": command not found" << std::endl;
         }
     }
