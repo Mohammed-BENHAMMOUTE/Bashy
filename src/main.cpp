4a9764ca503f6dbf2ad490873d7c8e8cf16acd18
Mohammed-BENHAMMOUTE <mohammedbenhammoute@gmail.com>
2024-12-06 15:33:59 +0100

    -adding the environment variable PATH to look for executables

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <algorithm>
 #include <iostream>
+#include <vector>
+#include <filesystem>
 using namespace std;
+namespace fs = std::filesystem;
 
-std::pair<std::string,std::string> processString(std::string& s) {
+bool findFile(const string& directory, const string& fileName) {
+    fs::path file_path = fs::path(directory) / fileName;
+    return fs::exists(file_path);
+}
+
+std::pair<std::string, std::string> processString(std::string& s) {
     std::pair<std::string, std::string> p;
     s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
         return !std::isspace(ch);
@@ -10,7 +18,7 @@ std::pair<std::string,std::string> processString(std::string& s) {
     s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
         return !std::isspace(ch);
     }));
-    auto it  = s.find(' ');
+    auto it = s.find(' ');
     if (it != std::string::npos) {
         p.first = s.substr(0, it);
         p.second = s.substr(it + 1);
@@ -19,30 +27,62 @@ std::pair<std::string,std::string> processString(std::string& s) {
     }
     return p;
 }
+
+vector<string> tokenizePath(const string& DELIMITER, string& PATH) {
+    vector<string> vec;
+    size_t start = 0;
+    size_t end = PATH.find(DELIMITER);
+    while (end != std::string::npos) {
+        vec.push_back(PATH.substr(start, end - start));
+        start = end + DELIMITER.length();
+        end = PATH.find(DELIMITER, start);
+    }
+    vec.push_back(PATH.substr(start, end));
+    return vec;
+}
+
 int main() {
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-   std::string input;
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+    std::string input;
+
+    // string PATH = "/usr/bin:/usr/local/bin";
+    string PATH  = getenv("PATH");
+
+    const string DELIMITER = ":";
+    vector<string> v = tokenizePath(DELIMITER, PATH);
     while (true) {
         std::cout << "$ ";
         getline(std::cin, input);
-        if (input == "exit 0") {
-            exit(0);
-        }
         pair<string, string> processedstring = processString(input);
-        if (processedstring.first == "echo") {
-            cout << processedstring.second<<endl;
-        }else if (processedstring.first == "type") {
-            pair<string, string> ps = processString(processedstring.second);
-            if (ps.first == "echo" || ps.first == "exit" || ps.first == "type") {
-                cout << ps.first << " is a shell builtin" <<endl;
-            }else {
-                std::cout << processedstring.second << ": not found" << std::endl;
+        if (processedstring.first == "exit") {
+            if (!processedstring.second.empty() && all_of(processedstring.second.begin(), processedstring.second.end(), ::isdigit)) {
+                const int exitcode = std::stoi(processedstring.second);
+                exit(exitcode);
             }
-        }else if (!input.empty()) {
+            std::cout << input << ": command not found" << std::endl;
+        }
+
+        if (processedstring.first == "echo") {
+            cout << processedstring.second << endl;
+        } else if (processedstring.first == "type") {
+            pair<string, string> ps = processString(processedstring.second);
+            if (!ps.first.empty() && ps.second.empty()) {
+                bool found = false;
+                for (const string& directorie : v) {
+                    // cout << "handling the directorie case: " << directorie << endl;
+                    if (findFile(directorie, ps.first)) {
+                        cout << ps.first << " is " << directorie << "/" << ps.first << endl;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    cout << ps.first << ": not found" << std::endl;
+                }
+            }
+        } else if (!input.empty()) {
             std::cout << input << ": command not found" << std::endl;
         }
     }
 }
-
-
